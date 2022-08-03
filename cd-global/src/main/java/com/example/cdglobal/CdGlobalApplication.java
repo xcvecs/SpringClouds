2592e27441ffcb30534b86644ce1ddd61cc969c9
xcv <ecsexecxcv@outlook.com>
2022-08-03 17:21:33 +0800

    init

--- cd-global/src/main/java/com/example/cdglobal/CdGlobalApplication.java
+++ cd-global/src/main/java/com/example/cdglobal/CdGlobalApplication.java
@@ -8,6 +8,7 @@ public class CdGlobalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CdGlobalApplication.class, args);
+        System.out.println();
     }
 
 }
